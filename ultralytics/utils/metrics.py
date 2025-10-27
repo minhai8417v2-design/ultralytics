fbceb8f4eb4307ef6847cbdaf106d3cddb1001d6
AILab8417 <minhai8417@gmail.com>
2025-10-27 09:46:50 +0700

    Update metrics.py
    Signed-off-by: AILab8417 <minhai8417@gmail.com>

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -1590,3 +1590,117 @@ class OBBMetrics(DetMetrics):
         DetMetrics.__init__(self, names)
         # TODO: probably remove task as well
         self.task = "obb"
+
+
+############################################################## Inner-WIoU loss begin ########################################################################
+
+
+def bbox_in_Wiou(box1, box2, xywh=True, CIoU=False, InCIoU=False, WIoU=False, InWIoU=False, scale=False, eps=1e-7, ratio=1.0):
+    # Get the coordinates of bounding boxes
+    if xywh:  # transform from xywh to xyxy
+        (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
+        w1_, h1_, w2_, h2_ = w1 / 2, h1 / 2, w2 / 2, h2 / 2
+        b1_x1, b1_x2, b1_y1, b1_y2 = x1 - w1_, x1 + w1_, y1 - h1_, y1 + h1_
+        b2_x1, b2_x2, b2_y1, b2_y2 = x2 - w2_, x2 + w2_, y2 - h2_, y2 + h2_
+    else:  # x1, y1, x2, y2 = box1
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1.chunk(4, -1)
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2.chunk(4, -1)
+        w1, h1 = b1_x2 - b1_x1, (b1_y2 - b1_y1) + eps   # .clamp(eps)    + eps
+        w2, h2 = b2_x2 - b2_x1, (b2_y2 - b2_y1) + eps       # .clamp(eps)       + eps
+
+    innner_iou = get_inner_iou(box1, box2, xywh=xywh, ratio=ratio)
+
+    # Intersection area
+    inter = (b1_x2.minimum(b2_x2) - b1_x1.maximum(b2_x1)).clamp_(0) * \
+            (b1_y2.minimum(b2_y2) - b1_y1.maximum(b2_y1)).clamp_(0)
+
+    # Union Area
+    union = w1 * h1 + w2 * h2 - inter + eps
+    
+    if scale:
+        #self = WIoU_Scale(1 - innner_iou)
+        self = WIoU_Scale(1.0 - (inter / union))
+    # IoU
+    iou = inter / (union ) #  + eps
+    if CIoU or WIoU or InWIoU or InCIoU:
+        cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
+        ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
+        c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
+        rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center dist ** 2
+        if CIoU:
+            v = (4 / math.pi ** 2) * (torch.atan(w2 / h2) - torch.atan(w1 / h1)).pow(2)
+            with torch.no_grad():
+                alpha_ciou = v / (v - iou + (1 + eps))
+            return iou - (rho2 / c2 + v * alpha_ciou) # CIoU
+        elif InCIoU:
+            v = (4 / math.pi ** 2) * (torch.atan(w2 / h2) - torch.atan(w1 / h1)).pow(2)
+            with torch.no_grad():
+                alpha_ciou = v / (v - iou + (1 + eps))
+            return innner_iou - (rho2 / c2 + v * alpha_ciou) # CIoU
+        elif WIoU: 
+            if scale:
+                #print('WIoU')
+                return getattr(WIoU_Scale, '_scaled_loss')(self), (1 - iou) * torch.exp((rho2 / c2)), iou # WIoU https://arxiv.org/abs/2301.10051   (1 - innner_iou)**2
+            else:
+                return iou, torch.exp((rho2 / c2)) # WIoU v1
+        elif InWIoU: 
+            if scale:
+                #print('InWIoU')
+                return getattr(WIoU_Scale, '_scaled_loss')(self), ((1 - innner_iou)) * torch.exp((rho2 / c2)), innner_iou # In-WIoU,使用 平方   **2
+            else:
+                return innner_iou, torch.exp((rho2 / c2)) # WIoU v1
+    return innner_iou  # IoU
+
+
+
+
+def get_inner_iou(box1, box2, xywh=True, eps=1e-7, ratio=0.7):
+    if not xywh:
+        box1, box2 = ops.xyxy2xywh(box1), ops.xyxy2xywh(box2)
+    (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
+    b1_x1, b1_x2, b1_y1, b1_y2 = x1 - (w1 * ratio) / 2, x1 + (w1 * ratio) / 2, y1 - (h1 * ratio) / 2, y1 + (h1 * ratio) / 2
+    b2_x1, b2_x2, b2_y1, b2_y2 = x2 - (w2 * ratio) / 2, x2 + (w2 * ratio) / 2, y2 - (h2 * ratio) / 2, y2 + (h2 * ratio) / 2
+    
+    # Intersection area
+    inter = (b1_x2.minimum(b2_x2) - b1_x1.maximum(b2_x1)).clamp_(0) * \
+            (b1_y2.minimum(b2_y2) - b1_y1.maximum(b2_y1)).clamp_(0)
+
+    # Union Area
+    union = w1 * h1 * ratio * ratio + w2 * h2 * ratio * ratio - inter + eps
+    return inter / union
+
+
+class WIoU_Scale:
+    ''' monotonous: {
+            None: origin v1
+            True: monotonic FM v2
+            False: non-monotonic FM v3
+        }
+        momentum: The momentum of running mean'''
+    
+    iou_mean = 1.
+    monotonous = False
+    _momentum = 1 - 0.5 ** (1 / 7000)
+    _is_train = True
+
+    def __init__(self, iou):
+        self.iou = iou
+        self._update(self)
+    
+    @classmethod
+    def _update(cls, self):
+        if cls._is_train: cls.iou_mean = (1 - cls._momentum) * cls.iou_mean + \
+                                         cls._momentum * self.iou.detach().mean().item()
+    
+    @classmethod
+    def _scaled_loss(cls, self, gamma=1.9, delta=3):
+        if isinstance(self.monotonous, bool):
+            if self.monotonous:    #使用单调
+                return (self.iou.detach() / self.iou_mean).sqrt()
+            else:                  # 使用非单调
+                beta = self.iou.detach() / self.iou_mean
+                alpha = delta * torch.pow(gamma, beta - delta)
+                return beta / alpha
+        return 1
+
+############################################################## Inner-WIoU loss end  #########################################################################
