bf5f3c9c16d36729d3f02d5fa6b37fe8117966e1
AILab8417 <minhai8417@gmail.com>
2025-10-30 18:42:58 +0700

    Update block.py
    Signed-off-by: AILab8417 <minhai8417@gmail.com>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2129,3 +2129,144 @@ class C2f_DCNv2(C2f):
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
         super().__init__(c1, c2, n, shortcut, g, e)
         self.m = nn.ModuleList(Bottleneck_DCNV2(self.c, self.c, shortcut, g, k=(3, 3), e=1.0) for _ in range(n))
+
+
+####################################################### CSRG ##################################################################
+class RepGhostModule(nn.Module):
+    def __init__(
+            self, inp, oup, kernel_size=1, dw_size=3, stride=1, relu=True, deploy=False, reparam_bn=True,
+            reparam_identity=False
+    ):
+        super(RepGhostModule, self).__init__()
+        init_channels = oup
+        new_channels = oup
+        self.deploy = deploy
+
+        self.primary_conv = nn.Sequential(
+            nn.Conv2d(
+                inp, init_channels, kernel_size, stride, kernel_size // 2, bias=False,
+            ),
+            nn.BatchNorm2d(init_channels),
+            nn.SiLU(inplace=True) if relu else nn.Sequential(),
+        )
+        fusion_conv = []
+        fusion_bn = []
+        if not deploy and reparam_bn:
+            fusion_conv.append(nn.Identity())
+            fusion_bn.append(nn.BatchNorm2d(init_channels))
+        if not deploy and reparam_identity:
+            fusion_conv.append(nn.Identity())
+            fusion_bn.append(nn.Identity())
+
+        self.fusion_conv = nn.Sequential(*fusion_conv)
+        self.fusion_bn = nn.Sequential(*fusion_bn)
+
+        self.cheap_operation = nn.Sequential(
+            nn.Conv2d(
+                init_channels,
+                new_channels,
+                dw_size,
+                1,
+                dw_size // 2,
+                groups=init_channels,
+                bias=deploy,
+            ),
+            nn.BatchNorm2d(new_channels) if not deploy else nn.Sequential(),
+            # nn.ReLU(inplace=True) if relu else nn.Sequential(),
+        )
+        if deploy:
+            self.cheap_operation = self.cheap_operation[0]
+        if relu:
+            self.relu = nn.SiLU(inplace=False)
+        else:
+            self.relu = nn.Sequential()
+
+    def forward(self, x):
+        x1 = self.primary_conv(x)  # mg
+        x2 = self.cheap_operation(x1)
+        for conv, bn in zip(self.fusion_conv, self.fusion_bn):
+            x2 = x2 + bn(conv(x1))
+        return self.relu(x2)
+
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.cheap_operation[0], self.cheap_operation[1])
+        for conv, bn in zip(self.fusion_conv, self.fusion_bn):
+            kernel, bias = self._fuse_bn_tensor(conv, bn, kernel3x3.shape[0], kernel3x3.device)
+            kernel3x3 += self._pad_1x1_to_3x3_tensor(kernel)
+            bias3x3 += bias
+        return kernel3x3, bias3x3
+
+    @staticmethod
+    def _pad_1x1_to_3x3_tensor(kernel1x1):
+        if kernel1x1 is None:
+            return 0
+        else:
+            return torch.nn.functional.pad(kernel1x1, [1, 1, 1, 1])
+
+    @staticmethod
+    def _fuse_bn_tensor(conv, bn, in_channels=None, device=None):
+        in_channels = in_channels if in_channels else bn.running_mean.shape[0]
+        device = device if device else bn.weight.device
+        if isinstance(conv, nn.Conv2d):
+            kernel = conv.weight
+            assert conv.bias is None
+        else:
+            assert isinstance(conv, nn.Identity)
+            kernel_value = np.zeros((in_channels, 1, 1, 1), dtype=np.float32)
+            for i in range(in_channels):
+                kernel_value[i, 0, 0, 0] = 1
+            kernel = torch.from_numpy(kernel_value).to(device)
+
+        if isinstance(bn, nn.BatchNorm2d):
+            running_mean = bn.running_mean
+            running_var = bn.running_var
+            gamma = bn.weight
+            beta = bn.bias
+            eps = bn.eps
+            std = (running_var + eps).sqrt()
+            t = (gamma / std).reshape(-1, 1, 1, 1)
+            return kernel * t, beta - running_mean * gamma / std
+        assert isinstance(bn, nn.Identity)
+        return kernel, torch.zeros(in_channels).to(kernel.device)
+
+    def switch_to_deploy(self):
+        if len(self.fusion_conv) == 0 and len(self.fusion_bn) == 0:
+            return
+        kernel, bias = self.get_equivalent_kernel_bias()
+        self.cheap_operation = nn.Conv2d(in_channels=self.cheap_operation[0].in_channels,
+                                         out_channels=self.cheap_operation[0].out_channels,
+                                         kernel_size=self.cheap_operation[0].kernel_size,
+                                         padding=self.cheap_operation[0].padding,
+                                         dilation=self.cheap_operation[0].dilation,
+                                         groups=self.cheap_operation[0].groups,
+                                         bias=True)
+        self.cheap_operation.weight.data = kernel
+        self.cheap_operation.bias.data = bias
+        self.__delattr__('fusion_conv')
+        self.__delattr__('fusion_bn')
+        self.fusion_conv = []
+        self.fusion_bn = []
+        self.deploy = True
+
+class SRG_Bottleneck(nn.Module):
+
+  def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3)):
+    super().__init__()
+    if c2 != c1:
+      c2 = c1
+    c_ = c2//2
+    self.repghostconv1 = RepGhostModule(c_, c_)
+    self.repghostconv2 = RepGhostModule(c2, c2)
+    self.add = shortcut and c1 == c2
+  
+  def forward(self, x):
+    x1, x2 = torch.chunk(x, 2, dim = 1)
+    x1 = self.repghostconv1(x1)
+    x_ = self.repghostconv2(torch.cat((x1, x2), dim = 1))
+    return x_
+
+class CSRG(C3):
+    def __init__(self, c1, c2, n=3, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)
+        self.m = nn.Sequential(*(SRG_Bottleneck(c_, c_) for _ in range(n)))
